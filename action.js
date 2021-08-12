d88e40c21fbb090786735ec972099323eca0cf57
Anubhav Sharma <65497015+Anubhav-kaushik@users.noreply.github.com>
2021-08-12 23:56:18 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,19 @@
+let one = document.querySelector('#first');
+let two = document.querySelector('#second');
+let three = document.querySelector('#third');
+let four = document.querySelector('#fourth');
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function action() {
+    const allEle = [one, two, three, four];
+    const times = [0, 0, 0, 0];
+
+    for (let i = 0; i < allEle.length; i++) {
+        i.classList.toggle('hide');
+        await sleep(times[i]);
+    }
+
+}
